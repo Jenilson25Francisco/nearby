49ca3e76df28752098bb0247d58cc2533ac4588c
jenilson25francisco <franciscoviwaldo@gmail.com>
2024-12-29 19:42:08 +0100

    feat: testing biomejs

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,8 @@
-import { Steps } from "@/components/steps";
-import { Welcome } from "@/components/welcome";
-import { Text, View } from "react-native";
+import { Steps } from '@/components/steps'
+import { Welcome } from '@/components/welcome'
+import { Text, View } from 'react-native'
 
-export default function Index(){
+export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
